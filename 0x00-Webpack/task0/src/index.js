88853c04b05dd800c67add744fc02f7621418f54
dina <dinahamadi.20@gmail.com>
2024-10-28 23:21:03 +0100

    task 0 part 1

---
+++ 0x00-Webpack/task0/src/index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+$(document).ready(function() {
+  // Add three paragraphs to the page body
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+});
